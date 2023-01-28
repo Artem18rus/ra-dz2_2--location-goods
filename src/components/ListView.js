826b2a823ddb8f600ef46f627ef634fd6469705c
Artem18rus <urart88@mail.ru>
2023-01-28 12:52:23 +0400

    3

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,22 +1,19 @@
 import React from "react";
 
-// function ListView(props) {
-//   return <div className="listView">{/* {console.log(props.item)} */}</div>;
-// }
-
 function ListView(props) {
   return (
     <ul className="listView">
-      {/* {console.log(props.item)} */}
-      {props.item.map((el, idx) => (
-        <li className="shopItem" key={idx}>
-          <img src={el.img} alt="shopItem" />
-          <div className="name-view">{el.name}</div>
-          <div className="color-view">{el.color}</div>
-          <div className="price-view">${el.price}</div>
-          <div className="add_btn-view">Add to cart</div>
-        </li>
-      ))}
+      {props.item
+        ? props.item.map((el, idx) => (
+            <li className="shopItem" key={idx}>
+              <img src={el.img} alt="shopItem" />
+              <div className="name-view">{el.name}</div>
+              <div className="color-view">{el.color}</div>
+              <div className="price-view">${el.price}</div>
+              <div className="add_btn-view">Add to cart</div>
+            </li>
+          ))
+        : null}
     </ul>
   );
 }
--- src/components/Store.js
+++ src/components/Store.js
@@ -6,18 +6,13 @@ import ListView from "./ListView";
 
 function Store() {
   let [icon, setIcon] = useState("view_list");
-  // let [cards, setCards] = useState(products);
 
   let cards = products;
   let item = products;
 
   const onSwitch = (e) => {
     let buttonStatus;
-    // e === "view_list"
-    //   ? (buttonStatus = "view_module")
-    //   : (buttonStatus = "view_list");
-    // let iconState = document.querySelector('.material-icons');
-    // console.log(iconState)
+
     if (e === "view_list") {
       buttonStatus = "view_module";
     } else {
@@ -31,8 +26,8 @@ function Store() {
   return (
     <div className="app-products">
       <IconSwitch icon={icon} onSwitch={onSwitch} />
-      <CardsView cards={cards} />
-      <ListView item={item} />
+      <CardsView cards={icon === "view_list" ? cards : null} />
+      <ListView item={icon === "view_module" ? item : null} />
     </div>
   );
 }
